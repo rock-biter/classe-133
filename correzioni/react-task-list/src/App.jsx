6371b14bfe97008768a6164b1f3751aaaa418ba3
rock-biter <74297920+rock-biter@users.noreply.github.com>
2024-11-27 10:24:19 +0100

    ADD: correzione react task list

---
+++ correzioni/react-task-list/src/App.jsx
@@ -0,0 +1,107 @@
+import tasks from './tasks.js'
+
+function App() {
+
+  console.log(tasks)
+  // array con in_progress e backlog
+  const todoTasks = tasks.filter((task) => task.state === 'backlog' || task.state === 'in_progress').sort((taskA,taskB) => taskA.priority - taskB.priority)
+
+
+  // array con completed
+  const completedTasks = tasks.filter((task) => task.state === 'completed' ).sort((taskA,taskB) => taskA.priority - taskB.priority)
+
+  return (
+    <>
+      <header className="main-header">
+        <div className="container">
+          <h1>
+            Task Manager
+          </h1>
+        </div>
+      </header>
+      <main>
+        <section className="tasks-section">
+          <div className="container">
+            <h2 className='task-title'>Current Task ({todoTasks.length})</h2>
+          </div>
+          <div className="container">
+            <ul className="task-list">
+              {todoTasks.map((task) => {
+
+                const { id, title, state, priority, estimatedTime} = task
+
+                let stateMessage = state === 'backlog' ? state : 'in progress'
+
+                // switch(state) {
+                //   case 'backlog': 
+                //     stateMessage = state
+                //     // esegui questo codice
+                //     break;
+                //   case 'in_progress':
+                //     // esegui questo codice
+                //     stateMessage = 'in progress'
+                //     break;
+                //   case 'completed':
+                //     // esegui questo codice
+                //     stateMessage = state
+                //     break;
+                // }
+
+                // let stateMessage = ''
+
+                // if(state === 'backlog') {
+                //   stateMessage = 'backlog'
+                // } else {
+                //   stateMessage = 'in progress'
+                // }
+
+                return <li key={id} className="task-item">
+                  <div className="task-item__head">
+                    <span className="task-title">{title}</span>
+                    <span className="task-state">{stateMessage}</span>
+                  </div>
+                  <div>
+                    Priority: {priority}
+                  </div>
+                  <div>
+                    estimated time: {estimatedTime} min
+                  </div>
+                </li>
+
+              })}
+            </ul>
+          </div>
+        </section>
+        <section className="tasks-section">
+          <div className="container">
+            <h2 className='task-title'>Completed Task ({completedTasks.length})</h2>
+          </div>
+          <div className="container">
+            <ul className="task-list">
+              {completedTasks.map((task) => {
+
+                const { id, title, state, priority, estimatedTime} = task
+
+                return <li key={id} className="task-item">
+                  <div className="task-item__head">
+                    <span className="task-title">{title}</span>
+                    <span className="task-state">{state}</span>
+                  </div>
+                  <div>
+                    Priority: {priority}
+                  </div>
+                  <div>
+                    estimated time: {estimatedTime} min
+                  </div>
+                </li>
+
+              })}
+            </ul>
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ correzioni/react-task-list/src/tasks.js
@@ -0,0 +1,76 @@
+const tasks = [
+	{
+		id: 1,
+		title: 'Implementare la homepage',
+		priority: 1,
+		estimatedTime: 120,
+		state: 'completed',
+	},
+	{
+		id: 2,
+		title: 'Sviluppare il menu di navigazione',
+		priority: 2,
+		estimatedTime: 60,
+		state: 'completed',
+	},
+	{
+		id: 3,
+		title: 'Creare il footer del sito',
+		priority: 3,
+		estimatedTime: 30,
+		state: 'completed',
+	},
+	{
+		id: 4,
+		title: 'Ottimizzare le performance',
+		priority: 1,
+		estimatedTime: 180,
+		state: 'completed',
+	},
+	{
+		id: 5,
+		title: 'Scrivere test per i componenti',
+		priority: 2,
+		estimatedTime: 90,
+		state: 'completed',
+	},
+	{
+		id: 6,
+		title: 'Implementare la pagina dei contatti',
+		priority: 3,
+		estimatedTime: 60,
+		state: 'completed',
+	},
+	{
+		id: 7,
+		title: 'Aggiungere animazioni CSS',
+		priority: 2,
+		estimatedTime: 30,
+		state: 'backlog',
+	},
+	{
+		id: 8,
+		title: "Integrare l'API di autenticazione",
+		priority: 1,
+		estimatedTime: 120,
+		state: 'in_progress',
+	},
+	{
+		id: 9,
+		title: 'Aggiornare la documentazione',
+		priority: 3,
+		estimatedTime: 60,
+		state: 'backlog',
+	},
+	{
+		id: 10,
+		title: 'Rifattorizzare il codice CSS',
+		priority: 2,
+		estimatedTime: 90,
+		state: 'in_progress',
+	},
+]
+
+// console.log(tasks)
+
+export default tasks
