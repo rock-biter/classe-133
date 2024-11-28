0d856df4a5e725a259fb2459ea27275de0208bf4
rock-biter <74297920+rock-biter@users.noreply.github.com>
2024-11-28 12:56:57 +0100

    refactoring button

--- react-pizzeria/src/components/ui/Button/Button.jsx
+++ react-pizzeria/src/components/ui/Button/Button.jsx
@@ -1,5 +1,9 @@
 import style from './Button.module.css'
 
 export default function Button({ className = '', dark = false , text = 'Testo pulsante' }) {
-  return <button className={`${style.button} ${ dark === true ? style.dark_button : style.light_button } ${className}`}>{text}</button>
+  return (
+    <button className={`${style.button} ${ dark === true ? style.dark_button : style.light_button } ${className}`}>
+      {text}
+    </button>
+  )
 }
