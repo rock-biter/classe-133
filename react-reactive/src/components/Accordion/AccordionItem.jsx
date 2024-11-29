41e710dd71ead0fa6a005175c9dd398c407f160e
rock-biter <74297920+rock-biter@users.noreply.github.com>
2024-11-29 13:05:03 +0100

    ADD: fontawesome

--- react-reactive/src/components/Accordion/AccordionItem.jsx
+++ react-reactive/src/components/Accordion/AccordionItem.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import style from './Accordion.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faMinus } from '@fortawesome/free-solid-svg-icons'
 
 export default function AccordionItem({ onToggle = () => {}, item, isOpen = false }) {
 
@@ -10,7 +13,9 @@ export default function AccordionItem({ onToggle = () => {}, item, isOpen = fals
       <div className={style.header}>
         <span className={style.title}>{title}</span>
         <span className={style.icon}>
-          { isOpen ? '-' : '+' }
+          {/* { isOpen ? '-' : '+' } */}
+          {/* <FontAwesomeIcon icon={faPlus} /> */}
+          <FontAwesomeIcon icon={isOpen ? faMinus : faPlus} />
         </span>
       </div>
       {isOpen && <div className={style.content}>
