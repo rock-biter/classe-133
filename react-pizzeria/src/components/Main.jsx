2bcf9fb514e0dc452d40c871db637ae142a6ce37
rock-biter <74297920+rock-biter@users.noreply.github.com>
2024-12-03 11:07:25 +0100

    ADD multiple form fields

--- react-pizzeria/src/components/Main.jsx
+++ react-pizzeria/src/components/Main.jsx
@@ -2,34 +2,70 @@ import Card from './pizzas/Card/Card';
 import initialPizzas from '../pizzas';
 import { useState } from 'react';
 
+const initialFormData = {
+  name: '',
+  image: undefined,
+  ingredients: '',
+  price: '',
+  isAvailable: false
+}
+
 export default function Main() {
 
   const [pizzas,setPizzas] = useState(initialPizzas)
-  const [price,setPrice] = useState('7')
-  const [name,setName] = useState('Napoli')
+  // const [price,setPrice] = useState('7')
+  // const [name,setName] = useState('Napoli')
+
+  const [formData,setFormData] = useState(initialFormData)
+
+  function handleFormData(e) {
+
+    console.log(e.target.name,e.target.value)
+    const key = e.target.name
+    const value = e.target.value
+
+    // con for in per duplicare l'oggetto
+    const newFormData = {}
+    for (const k in formData) {
+      newFormData[k] = formData[k]
+    }
+    newFormData[key] = value
+
+    // con spread in 3 passaggi
+    // const newFormData = {...formData}
+    // newFormData[key] = value
+
+    // con spread in 1 solo passaggio
+    // const newFormData = {
+    //   ...formData,
+    //   [key]: value,
+    // }
+
+    setFormData(newFormData)
+
+    // console.log(newFormData)
+  }
 
   // featch delle pizze dal server
 
   function addPizza(e) {
     e.preventDefault()
 
-    const newName = name.trim()
-    const newPrice = +price // parseInt(price)
+    // const newName = name.trim()
+    // const newPrice = +price // parseInt(price)
 
-    if(newName === '' || isNaN(newPrice)) return
+    if(formData.name.trim() === '' || isNaN(formData.price)) return
 
     const newPizza = {
       id: Date.now(),
-      name: newName,
-      image: undefined,
-      ingredients: [],
-      price: newPrice,
-      isAvailable: true
+      ...formData,
+      ingredients: formData.ingredients.split(',').map((ing) => ing.trim())
     }
 
     setPizzas([...pizzas,newPizza])
-    setName('')
-    setPrice('')
+    setFormData(initialFormData) // resettare il form
+    // setName('')
+    // setPrice('')
 
     // ...
 
@@ -44,9 +80,27 @@ export default function Main() {
           <p className='section-description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi repellat, harum molestiae nostrum provident laborum nobis doloribus perspiciatis..</p>
         </div>
         <div className="container">
-          <form onSubmit={addPizza} action="">
-            <input type="text" onChange={(e) => setName(e.target.value)} placeholder='Nome della pizza' value={name} />
-            <input type="text" onChange={(e) => setPrice(e.target.value)} placeholder='Prezzo' value={price}/>
+          
+          <form className='form-pizza' onSubmit={addPizza} action="">
+            <h2>Aggiungi una nuova pizza</h2>
+            {/* <input type="text" onChange={(e) => setName(e.target.value)} placeholder='Nome della pizza' value={name} />
+            <input type="text" onChange={(e) => setPrice(e.target.value)} placeholder='Prezzo' value={price}/> */}
+            <div className='form-field'>
+              <label htmlFor="name">Nome</label>
+              <input id="name" name='name' onChange={handleFormData} value={formData.name} type="text" placeholder='Nome della pizza' />
+            </div>
+            <div className='form-field'>
+              <label htmlFor="price">Prezzo</label>
+              <input id="price" name='price' onChange={handleFormData} value={formData.price} type="text" placeholder='Prezzo'/>
+            </div>
+            <div className='form-field'>
+              <label htmlFor="ingredients">Ingredienti</label>
+              <input id="ingredients" name='ingredients' onChange={handleFormData} value={formData.ingredients} type="text" placeholder='Ingredienti'/>
+            </div>
+            <div className='form-field form-field-inline'>
+              <input onChange={handleFormData} name='isAvailable' id="avalable" type="checkbox"/>
+              <label htmlFor="avalable">Dipsonibilità</label>
+            </div>
             <input type="submit" value='Aggiungi' />
           </form>
         </div>
